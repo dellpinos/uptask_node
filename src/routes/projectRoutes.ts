c023c05220bdf2104ae9b8ce2f8a19e99cc715ff
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-11-09 17:34:46 -0300

    validation

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
+router.post('/', 
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente ess obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripción es obligatoria'),
+    handleInputErrors,
+    ProjectController.createProject
+);
+
 router.get('/', ProjectController.getAllProject);
-router.post('/', ProjectController.createProject);
 
 export default router;
