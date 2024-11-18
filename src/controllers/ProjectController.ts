b61dfd6738b40e9146983242834b04d11470f58a
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-11-18 13:54:11 -0300

    crud put

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -17,8 +17,40 @@ export class ProjectController {
 
     }
 
-    static getAllProject = async (req: Request, res: Response) => {
-        res.send('Todos los proyectos');
+    static getAllProjects = async (req: Request, res: Response) => {
+        try {
+            const projects = await Project.find({})
+            res.json(projects)
+        } catch (error) {
+            console.log(error);
+        }
     }
 
+    static getProjectById = async (req: Request, res: Response) => {
+
+        const { id } = req.params;
+        try {
+            const project = await Project.findById(id)
+
+            if(!project) {
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({ error: error.message })
+            }
+            res.json(project);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static updateProject = async (req: Request, res: Response) => {
+
+        const { id } = req.params;
+
+        try {
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
