85b4474aefdb2b0e24c8f3de42bd73d05d4e70f2
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-11-26 19:27:46 -0300

    middleware

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,20 @@
+import type { Response, Request } from "express";
+import Task from "../models/Task";
+
+
+export class TaskController {
+
+    static createTask = async (req: Request, res: Response) => {
+        try {
+            const task = new Task(req.body);
+            task.project = req.project.id;
+            req.project.tasks.push(task.id);
+            await task.save();
+            await req.project.save();
+            res.send('Tarea creada correctamente')
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware/validation';
+import { TaskController } from '../controllers/TaskController';
+import { validateProjectExists } from '../middleware/project';
 
 const router = Router();
 
@@ -40,6 +42,13 @@ router.delete('/:id',
     param('id').isMongoId().withMessage('ID no válido'),
     handleInputErrors,
     ProjectController.deleteProject
+);
+
+/** Routes for Task */
+
+router.post('/:projectId/tasks',
+    validateProjectExists,
+    TaskController.createTask
 );
 
 export default router;
