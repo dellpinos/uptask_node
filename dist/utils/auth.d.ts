95f7d26f0c6b277f6b6b846840cc7dfa2a6b9b31
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2025-08-20 21:20:10 -0300

    mongoose updated

--- dist/utils/auth.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const hashPasword: (password: string) => Promise<string>;
-export declare const checkPassword: (enteredPassword: string, storedPassword: string) => Promise<boolean>;
--- dist/utils/token.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generate6digitToken = void 0;
-const generate6digitToken = () => Math.floor(100000 + Math.random() * 900000).toString();
-exports.generate6digitToken = generate6digitToken;
-//# sourceMappingURL=token.js.map
